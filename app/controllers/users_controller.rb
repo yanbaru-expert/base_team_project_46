e50e542114cb2848e180e5020948e149f48bfb51
hayato-1221 <hiperformance888@gmail.com>
2020-12-17 17:31:46 +0900

    一覧表示機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -8,6 +12,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :age)
   end
